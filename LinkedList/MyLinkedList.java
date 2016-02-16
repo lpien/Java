18ab7e0a44235239ba9faab1c8f797661bcce783
lpien <lpien@u.rochester.edu>
2016-02-16 16:27:21 -0500

    Create MyLinkedList.java

---
+++ LinkedList/MyLinkedList.java
@@ -0,0 +1,8 @@
+public interface MyLinkedList {
+	  public void insert(Object x);
+	  public void delete(Object x);
+	  public boolean lookup(Object x);
+	  public boolean isEmpty();
+	  public void printList();
+
+}
